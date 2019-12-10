50415a6e70ca0d158e542f502b73e2111bdc21f2
Pouya <pamid@uvic.com>
2019-12-10 14:58:08 -0800

    form is made, request can be send back as part o body to be shown in a url as well as be printed in the terminal. Use of body-parser module is implemented.

--- express_server.js
+++ express_server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended :true}));
 app.set("view engine","ejs");
+
+
+function generateRandomString(outputLength) {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < outputLength; i ++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  console.log('six digit code: ', result);
+  return result;
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,18 +38,26 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// new route submission form :
+app.get("/urls/new", (req,res)=>{ 
+  res.render("urls_new");
+})
+// the form in the urls/new generates 
+app.post("/urls" ,(req,res) => {
+  console.log(req.body);
+  res.send(req.body.longURL);
+})
 
 app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
+
   // res.render("urls_index", urlDatabase);
 });
 
-app.get("/urls", (req, res) => {
-  let templateVars = {urls: urlDatabase};
-  res.render("urls_index", templateVars);
-  // res.render("urls_index", urlDatabase);
-});
+
+
+
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {shortURL: req.params.shortURL,
@@ -44,3 +65,4 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
   // res.render("urls_index", urlDatabase);
 });
+
